dba55a31cde65c85600e8ca12a348dd035c85e66
chaoqin-li1123 <chaoqinli@google.com>
2021-11-17 21:21:35 +0000

    fi readme and add client retry
    Signed-off-by: chaoqin-li1123 <chaoqinli@google.com>

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # ToyServer
-To build the server
+To build the server with bazel
 bazel build //:server_main
 To build the client
 bazel build //:client_main
 
 To run the server
-./bazel-bin/server_main port
+./bazel-bin/server_main hostname port
 
 To run the client
 ./bazel-bin/client_main hostname port
+
+To build the server with make
+make
+
+To run the server
+server_main hostname port
+client_main hostname port
+
--- client_main.cc
+++ client_main.cc
@@ -1,6 +1,9 @@
+#include <chrono>
 #include <csignal>
+#include <thread>
 
 #include "client.h"
+#include "utility.h"
 
 static Client::Client* client_ptr{nullptr};
 
@@ -10,9 +13,24 @@ static void signalHandler(int signal_number) {
 }
 
 int main(int argc, char* argv[]) {
-  Client::Client client(argv[1], argv[2]);
-  client_ptr = &client;
+  auto retry_interval = std::chrono::microseconds(1000);
+  int retries = 0;
+  while (retries < 6) {
+    try {
+      client_ptr = new Client::Client(argv[1], argv[2]);
+      std::cout << "connect successfully" << std::endl;
+      break;
+    } catch (Utility::NetworkException& e) {
+      delete client_ptr;
+      std::this_thread::sleep_for(retry_interval);
+      retry_interval *= 2;
+      continue;
+    }
+  }
+  if (!client_ptr) {
+    exit(1);
+  }
   signal(SIGTERM, signalHandler);
   signal(SIGINT, signalHandler);
-  client.run();
+  client_ptr->run();
 }
